20a6d17344803b3373005194ea5fb637edadf857
chris-etheridge <chris.etheridge@cognician.com>
2016-05-06 14:52:55 +0200

    Cleanup of project.clj

--- project.clj
+++ project.clj
@@ -5,14 +5,21 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.6.1"
-  
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [org.clojure/core.async "0.2.374"
-                  :exclusions [org.clojure/tools.reader]]]
-  
+                  :exclusions [org.clojure/tools.reader]]
+                 [com.datomic/datomic-pro "0.9.5206"
+                  :exclusions [joda-time]]
+                 [enlive "1.1.6"]
+                 [compojure "1.5.0"]
+                 [datascript "0.15.0"]
+                 [org.omcljs/om "1.0.0-alpha34"]]
+
   :plugins [[lein-figwheel "0.5.3"]
-            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
+            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]
+            [lein-scss "0.2.4"]]
 
   :source-paths ["src"]
 
@@ -22,7 +29,6 @@
               [{:id "dev"
                 :source-paths ["src"]
 
-                ;; If no code is to be run, set :figwheel true for continued automagical reloading
                 :figwheel {:on-jsload "playground.core/on-js-reload"}
 
                 :compiler {:main playground.core
@@ -30,9 +36,6 @@
                            :output-to "resources/public/js/compiled/playground.js"
                            :output-dir "resources/public/js/compiled/out"
                            :source-map-timestamp true}}
-               ;; This next build is an compressed minified build for
-               ;; production. You can build this with:
-               ;; lein cljsbuild once min
                {:id "min"
                 :source-paths ["src"]
                 :compiler {:output-to "resources/public/js/compiled/playground.js"
@@ -40,52 +43,11 @@
                            :optimizations :advanced
                            :pretty-print false}}]}
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
+  :figwheel {:css-dirs ["resources/public/css"]}
 
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :) (see lien-ring)
-             
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-             
-             ;; if you are using emacsclient you can just use
-             ;; :open-file-command "emacsclient"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             }
-
- 
-  ;; setting up nREPL for Figwheel and ClojureScript dev
-  ;; Please see:
-  ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-  
   :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.3"]
                                   [com.cemerick/piggieback "0.2.1"]]
-                   ;; for CIDER
-                   ;; :plugins [[cider/cider-nrepl "0.12.0"]]
-                   :repl-options {; for nREPL dev you really need to limit output
-                                  :init (set! *print-length* 50)
+                   :repl-options {:init (set! *print-length* 50)
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
 
 )
