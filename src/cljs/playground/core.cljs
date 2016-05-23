fa389763ed2d773b2cf49f7cbb8bd9a97027e0c9
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-23 22:31:11 +0200

    Cleaned up old, copied code

--- src/cljs/playground/core.cljs
+++ src/cljs/playground/core.cljs
@@ -3,26 +3,7 @@
             [datascript.core :as d]
             [cljs.core.async :as async]))
 
-(enable-console-print!)
-
-;; schema
-
-(def schema {;; room schema
-              :room/title {}
-              ;; message schema
-              :message/room {:db/valueType :db.type/ref}
-              :message/text {}
-              :message/author {:db/valueType :db.type/ref}
-              :message/timestamp {}
-              :message/unread {}
-              ;; user schema
-              :user/name {}
-              :user/avatar {}
-              :user/me {}
-              :user/state {}})
-
-(def conn (d/create-conn schema))
-
-
-
+;;; For a chat app I need the following:
+;;; - chat pane with all messages
+;;; - compose pane to accept input
 
