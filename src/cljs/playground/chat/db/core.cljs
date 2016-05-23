e2afeafd51d7a2788e5a30ffbf077f4d9314d9a1
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-23 22:30:27 +0200

    Added chat core stub

---
+++ src/cljs/playground/chat/db/core.cljs
@@ -0,0 +1,24 @@
+(ns playground.chat.db.core
+  (:require [datascript.core :as d]
+            [cljs.core.async :as async]))
+
+(enable-console-print!)
+
+;; schema
+
+(def schema {;; message schema
+              :message/text {}
+              :message/timestamp {}
+              :message/unread {}
+              ;; user schema
+              :user/name {}
+              :user/avatar {}
+              :user/me {}
+              :user/state {}})
+
+(defn setup-db! []
+  (d/create-conn schema))
+
+
+
+
