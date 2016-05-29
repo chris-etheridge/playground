415c32b76d62ef1e42c93d65eccbdab46041f367
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-29 20:50:53 +0200

    Adding, completing, rendering todos

--- src/cljs/playground/devcards/cards/todo.cljs
+++ src/cljs/playground/devcards/cards/todo.cljs
@@ -1,5 +1,6 @@
 (ns playground.devcards.cards.todo
   (:require [rum.core :as rum]
+            [playground.util :as util]
             [cljs.core.async :as async]
             [devcards.core :as dc])
   (:require-macros [cljs.core.async.macros :refer [go]]
@@ -24,30 +25,81 @@
      :history      true})
 
 (def *app-state (atom {:user/name  "Chris"
-                       :user/todos [{:id         356
-                                     :text       "Take out the trash!"
-                                     :completed? false}
-                                    {:id         357
-                                     :text       "Clean the dog wee."
-                                     :completed? true}
-                                    {:id         359
-                                     :text       "Drink some water."
-                                     :completed? false}]}))
+                       :user/todos {457 {:text       "Take out the trash!"
+                                         :completed? false}
+                                    458 {:text       "Clean the dog wee."
+                                         :completed? true}
+                                    459 {:text       "Drink some water."
+                                         :completed? false}}}))
 
-(rum/defc one-todo-card [data]
-  [:div.row
-   [:.col-md-6
-    [:.input-group
-     [:span.input-group-addon
-      [:input {:type "checkbox"}]]
-     (:text @data)]]])
+(defn toggle-todo [tid done?]
+  (swap! *app-state assoc-in [:user/todos tid :completed?] (not done?)))
+
+(defn next-todo-id []
+  (-> @*app-state
+      :user/todos
+      count
+      inc))
+
+(defn add-todo [text]
+  (let [todo {:text       text
+              :completed? false}]
+    (swap! *app-state assoc-in [:user/todos (next-todo-id)] todo)))
+
+(rum/defc todo [data]
+  [:.row.todo
+   [:.col-md-1
+    [:input {:type "checkbox"}]]
+   [:.col-md-11
+    [:p (:text data)]]])
+
+
+(rum/defc todo-re [[tid todo]]
+  [:.row.todo {:class (when (:completed? todo) "complete")}
+   [:.col-md-1
+    [:input {:type      "checkbox"
+             :checked   (:completed? todo)
+             :on-change #(toggle-todo tid (:completed? todo))
+             :class     (when (:completed? todo)
+                          "complete")}]]
+   [:.col-md-10
+    [:p (:text todo)]]])
+
+(rum/defc todo-list [data]
+  ;;(prn (:user/todos @data))
+  [:row.todo__cards
+   (map #(todo  %) (:user/todos @data))])
+
+(rum/defc todo-list-re < rum/reactive [todos-ref]
+  [:row.todo__cards
+   (map #(todo-re %) (rum/react todos-ref))])
+
+(rum/defc add-todo-pane []
+  [:label "Enter a todo"]
+  [:input {:type        "text"
+           :on-key-down (util/on-textarea-keydown #(add-todo %))}])
 
 (defcard one-todo
   "A simple todo card"
   (fn [data]
-    (one-todo-card data))
+    (todo @data))
   {:id         1
    :text       "Take out the trash."
    :completed? false})
 
+(defcard todo-list
+  "List of todos. No interaction with app-state."
+  (todo-list *app-state))
+
+(defcard todo-list-reactive
+  "Complete 1 todo. Interacting with app-state."
+  (fn [state _]
+    (todo-list-re (rum/cursor state [:user/todos])))
+  *app-state
+  {:inspect-data true
+   :history      true})
+
+(defcard adding-a-todo
+  "Add a todo to app-state"
+  (add-todo-pane))
 
