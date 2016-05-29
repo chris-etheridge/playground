b85363177a607f878ddd5dbd23271c2218ad216e
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-29 14:02:29 +0200

    Initial devcards for todo

--- src/cljs/playground/devcards/cards/todo.cljs
+++ src/cljs/playground/devcards/cards/todo.cljs
@@ -6,4 +6,48 @@
                    [devcards.core :refer [defcard]]))
 
 
-(defcard todo-card "")
+(defcard todo-docs
+  "The global state will contain all of the todos that the user currently has.
+  Each todo will have the following structure:
+
+  ```clj
+  {:id 1
+   :text \"Take out the trash\"
+   :completed? false}
+  ```")
+
+
+#_(defcard with-opts
+    "Card with extra options."
+    {}
+    {:inspect-data true
+     :history      true})
+
+(def *app-state (atom {:user/name  "Chris"
+                       :user/todos [{:id         356
+                                     :text       "Take out the trash!"
+                                     :completed? false}
+                                    {:id         357
+                                     :text       "Clean the dog wee."
+                                     :completed? true}
+                                    {:id         359
+                                     :text       "Drink some water."
+                                     :completed? false}]}))
+
+(rum/defc one-todo-card [data]
+  [:div.row
+   [:.col-md-6
+    [:.input-group
+     [:span.input-group-addon
+      [:input {:type "checkbox"}]]
+     (:text @data)]]])
+
+(defcard one-todo
+  "A simple todo card"
+  (fn [data]
+    (one-todo-card data))
+  {:id         1
+   :text       "Take out the trash."
+   :completed? false})
+
+
